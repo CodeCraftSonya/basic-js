cd8ce29f01ef5a5cf0be50d0acf671e5decb9192
Sofia Orazklycheva <sofiyafish@yandex.ru>
2024-12-25 16:23:13 +0300

    feat: solve 8 tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (!options.separator) {options.separator = '+'}
+  if (options.addition === undefined) {options.addition = ''};
+  if (!options.additionSeparator) {options.additionSeparator = '|'};
+  if (typeof str !== 'string') {str = String(str)};
+  if (typeof options.separator !== 'string') {options.separator = String(options.separator)};
+  if (typeof options.additionSeparator !== 'string') {options.additionSeparator = String(options.additionSeparator)};
+  if (typeof options.addition !== 'string') {options.addition = String(options.addition)};
+
+
+
+  if (options.additionRepeatTimes > 1) {
+    let additionWithSep = options.addition + options.additionSeparator
+    let additionalEnd = additionWithSep.repeat(options.additionRepeatTimes - 1) + options.addition;
+    let fullPartOfStr = str + additionalEnd;
+    let fullPartOfStrWithSep = fullPartOfStr + options.separator;
+    return fullPartOfStrWithSep.repeat(options.repeatTimes - 1) + fullPartOfStr;
+  }
+  if (options.additionRepeatTimes === 1 || !options.additionRepeatTimes && options.addition) {
+    let fullPartOfStr = str + options.addition;
+    let fullPartOfStrWithSep = fullPartOfStr + options.separator;
+    return fullPartOfStrWithSep.repeat(options.repeatTimes - 1) + fullPartOfStr;
+  }
+  let strWithSep = str + options.separator;
+  return strWithSep.repeat(options.repeatTimes - 1) + str;
 }
 
 module.exports = {
