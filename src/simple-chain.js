5f1e3356115dff5eed754ef1624939687ab4d8d4
Sofia Orazklycheva <sofiyafish@yandex.ru>
2024-12-30 14:21:17 +0300

    feat: solve +2 tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    String.toString(value);
+    value = `( ${ value } )`;
+    this.arr.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position === 'number' && position <= this.arr.length && position > 0 && Number.isInteger(position)) {
+      this.arr.splice(position - 1, 1);
+    } else {
+      this.arr = [];
+      throw new Error('You can\'t remove incorrect link!')
+    }
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.arr.join('~~');
+    this.arr = [];
+    return result;
   }
 };
 
