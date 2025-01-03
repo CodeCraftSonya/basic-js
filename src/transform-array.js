d148f582996e7766a9f68df927e70cd306f0a8e2
Sofia Orazklycheva <sofiyafish@yandex.ru>
2025-01-03 11:23:57 +0300

    feat: solve +2 tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  } else{
+    const finalArr = Array.from(arr);
+    for (let i = 0; i < finalArr.length; i++){
+      if (finalArr[i] === '--discard-prev') {
+        if (finalArr[i - 1] !== ''){
+          if (finalArr[i - 1] !== undefined) {
+            finalArr.splice(i - 1, 2, '', '');
+          } else {
+            finalArr.splice(i, 1, '');
+          }
+        } else {
+          finalArr.splice(i, 1, '');
+        }
+      }
+
+      if (finalArr[i] === '--discard-next') {
+        if (finalArr[i + 1] !== '') {
+          if (finalArr[i + 1] !== undefined) {
+            finalArr.splice(i, 2, '', '');
+          } else {
+            finalArr.splice(i, 1, '');
+          }
+        }else {
+          finalArr.splice(i, 1, '');
+        }
+      }
+
+      if (finalArr[i] === '--double-prev') {
+        if (finalArr[i - 1] !== '') {
+          if (finalArr[i - 1] !== undefined) {
+            finalArr.splice(i, 1, finalArr[i - 1]);
+          } else {
+            finalArr.splice(i, 1);
+          }
+        }else {
+          finalArr.splice(i, 1, '');
+        }
+      }
+
+      if (finalArr[i] === '--double-next') {
+        if (finalArr[i + 1] !== '') {
+          if (finalArr[i + 1] !== undefined) {
+            finalArr.splice(i, 1, finalArr[i + 1]);
+          } else {
+            finalArr.splice(i, 1);
+          }
+        }else {
+          finalArr.splice(i, 1, '');
+        }
+      }
+    }
+    return finalArr.filter((item) => {
+      if (item === '') {
+        return false
+      } else {
+        return true
+      }
+    });
+  }
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,109 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+function CalculateCharPos(word, indexChar){
+  while(indexChar >= word.length){
+    indexChar -= word.length;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return indexChar;
+}
+
+function EncodeByCharacters(wordFrom,wordEncoded){
+  let finalStr = '';
+  for (let i =0;i < wordEncoded.length; i++){
+    if(!IsCharacter(wordEncoded[i])) {
+      finalStr+= wordEncoded[i];
+      continue;
+    }
+    const shift = wordEncoded.charCodeAt(i) - 'a'.charCodeAt();
+    let charCodeWithShift = wordFrom.charCodeAt(i) + shift;
+    if (charCodeWithShift > 'z'.charCodeAt()) {
+      const shiftForCycle = charCodeWithShift - 'z'.charCodeAt();
+      charCodeWithShift = 'a'.charCodeAt() + shiftForCycle - 1;
+    }
+    finalStr += String.fromCharCode(charCodeWithShift);
+  }
+
+  return finalStr;
+}
+
+function DecodeByCharacters(wordFrom,wordEncoded){
+  let finalStr = '';
+
+  for (let i =0, notCharCounter = 0;i < wordFrom.length; i++){
+
+    if(!IsCharacter(wordFrom[i])) {
+      finalStr+= wordFrom[i];
+      notCharCounter++;
+      continue;
+    }
+
+    let indexChar = CalculateCharPos(wordEncoded, i- notCharCounter);
+    const shift = wordEncoded.charCodeAt(indexChar) - 'a'.charCodeAt();
+    let charCodeWithShift = wordFrom.charCodeAt(i) - shift;
+
+    if (charCodeWithShift < 'a'.charCodeAt()) {
+      const shiftForCycle = 'a'.charCodeAt() - charCodeWithShift;
+      charCodeWithShift = 'z'.charCodeAt() - shiftForCycle + 1;
+    }
+
+    finalStr += String.fromCharCode(charCodeWithShift);
+  }
+
+  return finalStr;
+}
+
+function EncodeByEncoderCharactersIndexPos(wordFrom, wordEncoder){
+  let encodedStr = '';
+  for (let i = 0, counterNotCharacter = 0; i < wordFrom.length; i++){
+    if(!IsCharacter(wordFrom[i])) {
+      encodedStr += wordFrom[i];
+      counterNotCharacter++
+      continue;
+    }
+    let indexPos = CalculateCharPos(wordEncoder, i-counterNotCharacter);
+    encodedStr += wordEncoder[indexPos];
+  }
+  return encodedStr;
+}
+
+function IsCharacter(char){
+  return char.charCodeAt() <= 122 && char.charCodeAt() >= 97;
+}
+
+class VigenereCipheringMachine {
+  constructor(isDirect = true) {
+    this.isDirect = isDirect; // Сохраняем переданный параметр
+  }
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }else {
+      let messageToLC = message.toLowerCase();
+      let keyToLC = key.toLowerCase();
+      let encodedCharactersStr = EncodeByEncoderCharactersIndexPos(messageToLC,keyToLC);
+      let resultStr = EncodeByCharacters(messageToLC,encodedCharactersStr);
+
+      if (!this.isDirect) {
+        resultStr = resultStr.split('').reverse().join('');
+      }
+
+      return resultStr.toUpperCase();
+    }
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    } else {
+      let messageToLowercase = encryptedMessage.toLowerCase();
+      let decodedResult = DecodeByCharacters(messageToLowercase, key);
+
+      if (!this.isDirect) {
+        decodedResult = decodedResult.split('').reverse().join('');
+      }
+      return decodedResult.toUpperCase();
+    }
   }
 }
 
